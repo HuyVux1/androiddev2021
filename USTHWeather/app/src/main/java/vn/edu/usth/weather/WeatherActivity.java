b89dce8d0d1118c6e8311a8be7d4f8a7bb339379
Huy Vu <huy17987@gmail.com>
2020-12-14 10:36:33 +0700

    practical 123

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,50 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class WeatherActivity extends AppCompatActivity {
+    private static  final String TAG = "WeatherActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+
+        Log.i(TAG,"Created");
+
+    }
+    @Override
+    protected void  onStart(){
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
+    }
+}
