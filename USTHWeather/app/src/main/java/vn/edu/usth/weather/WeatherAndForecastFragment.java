ec7de393523ba25f91668b7c19399540593e78cf
Huy Vu <huy17987@gmail.com>
2021-01-06 22:04:23 +0700

    update lab 7

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherAndForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherAndForecastFragment.java
@@ -8,16 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WeatherAndForecastFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import vn.edu.usth.weather.R;
+
 public class WeatherAndForecastFragment extends Fragment {
+    public WeatherAndForecastFragment() {
+        // Required empty public constructor
+    }
 
-
-
-
+    public static WeatherAndForecastFragment newInstance() {
+        Bundle args = new Bundle();
+        WeatherAndForecastFragment fragment = new WeatherAndForecastFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
